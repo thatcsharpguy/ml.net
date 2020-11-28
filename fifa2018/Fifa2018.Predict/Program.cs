28abe43c0a534b49422ada5898a4d44ae3f48388
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-11-28 07:51:00 +0000

    FIFA 2018

---
+++ fifa2018/Fifa2018.Predict/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fifa2018.Predict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
