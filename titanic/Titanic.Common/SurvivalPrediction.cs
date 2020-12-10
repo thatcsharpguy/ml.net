8fa9f83113b181632ca36118797fedaf1beaf684
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-12-10 18:41:08 +0000

    Update codes: WIP

---
+++ titanic/Titanic.Common/SurvivalPrediction.cs
@@ -0,0 +1,11 @@
+namespace Titanic.Common
+{
+    public class SurvivalPrediction
+    {
+        public bool Label { get; set; }
+        public float Probability { get; set; }
+        public float Score { get; set; }
+    }
+}
+
+
